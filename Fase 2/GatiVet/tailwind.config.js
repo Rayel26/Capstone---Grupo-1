aaad34e34ff0b1e2d70fb4fc658b8326cc28df23
Rayel26 <102603847+Rayel26@users.noreply.github.com>
2024-08-29 18:28:05 -0400

    inicio pag, web
    Se inicia subiendo archivos página web "GatiVet"

---
+++ Fase 2/GatiVet/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './templates/**/*.html',
+      './static/styles/**/*.css',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+  
+  
