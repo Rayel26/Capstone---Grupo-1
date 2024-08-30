0605d4fc3a5b2180a8fdcf1c6db228c5452a6141
Rayel26 <102603847+Rayel26@users.noreply.github.com>
2024-08-30 13:56:59 -0400

    avance login

--- Fase 2/GatiVet/app.py
+++ Fase 2/GatiVet/app.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Ruta para la página de inicio
 @app.route('/')
 def home():
     return render_template('home.html')
 
+#Ruta para la página de inicio de sesión
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
