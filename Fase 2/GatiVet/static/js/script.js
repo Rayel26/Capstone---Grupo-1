1b5686e2ad9333b143363b175ce7db6f833013ee
Rayel26 <102603847+Rayel26@users.noreply.github.com>
2024-09-13 17:03:18 -0300

    home terminado

--- Fase 2/GatiVet/static/js/script.js
+++ Fase 2/GatiVet/static/js/script.js
@@ -43,12 +43,58 @@ function showContent(sectionId) {
     activeButton.querySelector('span').classList.add('active-step');
 }
 
-// Mostrar la sección de perfil por defecto
+    // Mostrar la sección de perfil por defecto
 showContent('profile');
 
-// Toggle sidebar collapse on smaller screens
+    // Toggle sidebar collapse on smaller screens
 function toggleSidebar() {
     document.getElementById('sidebar').classList.toggle('collapsed');
 }
 
 //fin perfil
+
+//Modales home
+//Modal ver más consulta general
+function openModal() {
+    document.getElementById('modal').classList.remove('hidden');
+}
+  
+function closeModal() {
+    document.getElementById('modal').classList.add('hidden');
+}
+//Fin modal ver más home 
+
+//Modal ver mas vacunación
+function openVaccinationModal() {
+    document.getElementById('vaccination-modal').classList.remove('hidden');
+}
+  
+function closeVaccinationModal() {
+    document.getElementById('vaccination-modal').classList.add('hidden');
+}
+//Fin modal ver mas vacunación
+
+//Modal ver mas toma de muestra
+function openSampleModal() {
+    document.getElementById('sample-modal').classList.remove('hidden');
+}
+  
+function closeSampleModal() {
+    document.getElementById('sample-modal').classList.add('hidden');
+}
+
+//Fin modal ver mas toma de muestra
+
+//Modal ver mas especialidad
+function openFelineModal() {
+    document.getElementById('feline-modal').classList.remove('hidden');
+}
+  
+function closeFelineModal() {
+    document.getElementById('feline-modal').classList.add('hidden');
+}
+//Fin modal ver mas especialidad
+
+//Fin modales home
+
+   
