694157f814ea72ed8f2c8ea2f3f251e3a5b8ff5f
Krulezitoh <89810104+JhonaKrul@users.noreply.github.com>
2024-09-16 19:39:05 -0300

    Se termina de corregir editar mascotas y carnet digital

--- Fase 2/GatiVet/static/js/profile.js
+++ Fase 2/GatiVet/static/js/profile.js
@@ -237,104 +237,105 @@ function showContent(sectionId) {
 
 ////////////////////////////////////////////////////////////////
 // Mascotas
-document.getElementById('pet-select').addEventListener('change', function() {
-    var select = this;
-    var selectedOption = select.options[select.selectedIndex];
-    
-    var name = selectedOption.getAttribute('data-name');
-    var age = selectedOption.getAttribute('data-age');
-    var species = selectedOption.getAttribute('data-species');
-    var breed = selectedOption.getAttribute('data-breed');
-    var birthdate = selectedOption.getAttribute('data-birthdate');
-    
-    var petName = document.getElementById('pet-name');
-    var petAge = document.getElementById('pet-age');
-    var petSpecies = document.getElementById('pet-species');
-    var petBreed = document.getElementById('pet-breed');
-    var petBirthdate = document.getElementById('pet-birthdate');
-    var editButton = document.getElementById('edit-button');
-    
-    if (name) {
-        petName.value = name;
-        petAge.value = age;
-        petSpecies.value = species;
-        petBreed.value = breed;
-        petBirthdate.value = birthdate;
-        editButton.disabled = false;
-    } else {
-        petName.value = 'Selecciona una mascota';
-        petAge.value = '';
-        petSpecies.value = '';
-        petBreed.value = '';
-        petBirthdate.value = '';
-        editButton.disabled = true;
-    }
-});
-
-// Selección de la imagen y vista previa
-document.getElementById('edit-photo-button').addEventListener('click', function() {
-    document.getElementById('pet-photo').click();
-});
-
-document.getElementById('pet-photo').addEventListener('change', function(e) {
-    const preview = document.getElementById('photo-preview');
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    
-    reader.onloadend = function () {
-        preview.src = reader.result;
-        preview.classList.remove('hidden');
-    };
-    
-    if (file) {
-        reader.readAsDataURL(file);
-    } else {
-        preview.src = "";
-        preview.classList.add('hidden');
-    }
-});
-
 document.addEventListener('DOMContentLoaded', function() {
-    // Abre el modal al hacer clic en el ícono "+"
-    document.getElementById('add-pet-icon').addEventListener('click', function() {
-        document.getElementById('add-pet-modal').classList.remove('hidden');
+    // Referencias a los elementos del formulario
+    const petSelect = document.getElementById('pet-select');
+    const petName = document.getElementById('pet-name');
+    const petAge = document.getElementById('pet-age');
+    const petSpecies = document.getElementById('pet-species');
+    const petBreed = document.getElementById('pet-breed');
+    const petBirthdate = document.getElementById('pet-birthdate');
+    const editButton = document.getElementById('edit-button');
+    const addPetIcon = document.getElementById('add-pet-icon');
+    const closeModal = document.getElementById('close-modal');
+    const addPetForm = document.getElementById('add-pet-form');
+    const addPetModal = document.getElementById('add-pet-modal');
+    const editPetModal = document.getElementById('edit-pet-modal');
+    const closeEditModal = document.getElementById('close-edit-modal');
+    const editPetForm = document.getElementById('edit-pet-form');
+    const editPetName = document.getElementById('edit-pet-name');
+    const editPetAge = document.getElementById('edit-pet-age');
+    const editPetSpecies = document.getElementById('edit-pet-species');
+    const editPetBreedDog = document.getElementById('edit-pet-breed-dog');
+    const editPetBreedCat = document.getElementById('edit-pet-breed-cat');
+    const editPetBirthdate = document.getElementById('edit-pet-birthdate');
+    const editPhotoButton = document.getElementById('edit-photo-button');
+    const petPhoto = document.getElementById('pet-photo');
+    const photoPreview = document.getElementById('photo-preview');
+    const newPetSpecies = document.getElementById('new-pet-species');
+    const newPetBreedDog = document.getElementById('new-pet-breed-dog');
+    const newPetBreedCat = document.getElementById('new-pet-breed-cat');
+
+    if (!petSelect || !petName || !petAge || !petSpecies || !petBreed || !petBirthdate || !editButton ||
+        !addPetIcon || !closeModal || !addPetForm || !addPetModal || !editPetModal || !closeEditModal ||
+        !editPetForm || !editPetName || !editPetAge || !editPetSpecies || !editPetBreedDog || !editPetBreedCat ||
+        !editPetBirthdate || !editPhotoButton || !petPhoto || !photoPreview || !newPetSpecies || !newPetBreedDog || !newPetBreedCat) {
+        console.error('Uno o más elementos necesarios no se encontraron en el DOM.');
+        return;
+    }
+
+    // Mostrar detalles de la mascota seleccionada
+    petSelect.addEventListener('change', function() {
+        const select = this;
+        const selectedOption = select.options[select.selectedIndex];
+        
+        const name = selectedOption.getAttribute('data-name');
+        const age = selectedOption.getAttribute('data-age');
+        const species = selectedOption.getAttribute('data-species');
+        const breed = selectedOption.getAttribute('data-breed');
+        const birthdate = selectedOption.getAttribute('data-birthdate');
+        
+        if (name) {
+            petName.value = name;
+            petAge.value = age;
+            petSpecies.value = species;
+            petBreed.value = breed;
+            petBirthdate.value = birthdate;
+            editButton.disabled = false;
+        } else {
+            petName.value = 'Selecciona una mascota';
+            petAge.value = '';
+            petSpecies.value = '';
+            petBreed.value = '';
+            petBirthdate.value = '';
+            editButton.disabled = true;
+        }
+    });
+
+    // Abre el modal para agregar una nueva mascota
+    addPetIcon.addEventListener('click', function() {
+        addPetModal.classList.remove('hidden');
     });
 
     // Cierra el modal al hacer clic en el botón "Cancelar"
-    document.getElementById('close-modal').addEventListener('click', function() {
-        document.getElementById('add-pet-modal').classList.add('hidden');
+    closeModal.addEventListener('click', function() {
+        addPetModal.classList.add('hidden');
     });
 
-    // Maneja el envío del formulario del modal
-    document.getElementById('add-pet-form').addEventListener('submit', function(event) {
+    // Maneja el envío del formulario del modal para agregar una nueva mascota
+    addPetForm.addEventListener('submit', function(event) {
         event.preventDefault();
         
         // Obtener valores del formulario
-        var name = document.getElementById('new-pet-name').value;
-        var age = document.getElementById('new-pet-age').value;
-        var species = document.getElementById('new-pet-species').value;
+        const name = document.getElementById('new-pet-name').value;
+        const age = document.getElementById('new-pet-age').value;
+        const species = document.getElementById('new-pet-species').value;
 
         // Seleccionar la raza correcta dependiendo de la especie
-        var breed;
+        let breed;
         if (species === 'perro') {
-            breed = document.getElementById('new-pet-breed-dog').value;
+            breed = newPetBreedDog.value;
         } else if (species === 'gato') {
-            breed = document.getElementById('new-pet-breed-cat').value;
+            breed = newPetBreedCat.value;
         } else {
             breed = ''; // En caso de otra especie
         }
 
-        var birthdate = document.getElementById('new-pet-birthdate').value;
-        
-        console.log('Nombre:', name);
-        console.log('Edad:', age);
-        console.log('Especie:', species);
-        console.log('Raza:', breed);
-        console.log('Fecha de Nacimiento:', birthdate);
+        const birthdate = document.getElementById('new-pet-birthdate').value;
         
         // Crear una nueva opción en el select
-        var select = document.getElementById('pet-select');
-        var newOption = document.createElement('option');
+        const select = petSelect;
+        const newOption = document.createElement('option');
         newOption.value = name.toLowerCase().replace(/\s+/g, '_'); // Genera un valor único para la opción
         newOption.textContent = name;
         newOption.setAttribute('data-name', name);
@@ -348,37 +349,143 @@ document.addEventListener('DOMContentLoaded', function() {
         select.value = newOption.value; // Seleccionar la nueva mascota automáticamente
 
         // Limpiar el formulario y ocultar el modal
-        document.getElementById('add-pet-form').reset();
-        document.getElementById('add-pet-modal').classList.add('hidden');
+        addPetForm.reset();
+        addPetModal.classList.add('hidden');
         
         // Actualizar campos de detalles de la mascota
-        document.getElementById('pet-name').value = name;
-        document.getElementById('pet-age').value = age;
-        document.getElementById('pet-species').value = species;
-        document.getElementById('pet-breed').value = breed;
-        document.getElementById('pet-birthdate').value = birthdate;
+        petName.value = name;
+        petAge.value = age;
+        petSpecies.value = species;
+        petBreed.value = breed;
+        petBirthdate.value = birthdate;
 
         // Habilitar el botón de editar
-        document.getElementById('edit-button').disabled = false;
+        editButton.disabled = false;
     });
-});
 
-// Razas
-document.getElementById('new-pet-species').addEventListener('change', function() {
-    var species = this.value;
-    var dogBreeds = document.getElementById('dog-breeds');
-    var catBreeds = document.getElementById('cat-breeds');
+    // Abre el modal para editar la mascota
+    editButton.addEventListener('click', function() {
+        const selectedOption = petSelect.options[petSelect.selectedIndex];
+        const name = selectedOption.getAttribute('data-name');
+        const age = selectedOption.getAttribute('data-age');
+        const species = selectedOption.getAttribute('data-species');
+        const breed = selectedOption.getAttribute('data-breed');
+        const birthdate = selectedOption.getAttribute('data-birthdate');
+
+        if (name) {
+            // Rellenar el formulario de edición con los datos de la mascota seleccionada
+            editPetName.value = name;
+            editPetAge.value = age;
+            editPetSpecies.value = species;
+            editPetSpecies.disabled = false; // Hacer el campo de especie editable para pruebas
+            editPetBreedDog.value = species === 'perro' ? breed : '';
+            editPetBreedCat.value = species === 'gato' ? breed : '';
+            editPetBirthdate.value = birthdate;
+
+            // Mostrar el modal
+            editPetModal.classList.remove('hidden');
+        } else {
+            console.error('No hay una mascota seleccionada para editar.');
+        }
+    });
+
+    // Cierra el modal de edición
+    closeEditModal.addEventListener('click', function() {
+        editPetModal.classList.add('hidden');
+    });
+
+    // Maneja el envío del formulario del modal de edición
+    editPetForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        // Obtener valores del formulario de edición
+        const name = editPetName.value;
+        const age = editPetAge.value;
+        const species = editPetSpecies.value;
+
+        // Seleccionar la raza correcta dependiendo de la especie
+        let breed;
+        if (species === 'perro') {
+            breed = editPetBreedDog.value;
+        } else if (species === 'gato') {
+            breed = editPetBreedCat.value;
+        } else {
+            breed = ''; // En caso de otra especie
+        }
+
+        const birthdate = editPetBirthdate.value;
+        
+        // Actualizar la opción en el <select> correspondiente
+        const select = petSelect;
+        const selectedOption = select.options[select.selectedIndex];
+        selectedOption.setAttribute('data-name', name);
+        selectedOption.setAttribute('data-age', age);
+        selectedOption.setAttribute('data-species', species);
+        selectedOption.setAttribute('data-breed', breed);
+        selectedOption.setAttribute('data-birthdate', birthdate);
+
+        // Actualizar los valores visibles en el select
+        selectedOption.textContent = name;
+
+        // Limpiar el formulario y ocultar el modal
+        editPetForm.reset();
+        editPetModal.classList.add('hidden');
+        
+        // Actualizar campos de detalles de la mascota
+        petName.value = name;
+        petAge.value = age;
+        petSpecies.value = species;
+        petBreed.value = breed;
+        petBirthdate.value = birthdate;
+
+        // Habilitar el botón de editar
+        editButton.disabled = false;
+    });
+
+    // Maneja la carga de foto
+    editPhotoButton.addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                photoPreview.src = e.target.result;
+                petPhoto.src = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        }
+    });
     
-    if (species === 'perro') {
-        dogBreeds.style.display = 'block';
-        catBreeds.style.display = 'none';
-    } else if (species === 'gato') {
-        catBreeds.style.display = 'block';
-        dogBreeds.style.display = 'none';
-    } else {
-        dogBreeds.style.display = 'none';
-        catBreeds.style.display = 'none';
-    }
+    // Función para actualizar las razas en función de la especie seleccionada en el formulario de edición
+    editPetSpecies.addEventListener('change', function() {
+        const species = editPetSpecies.value;
+
+        if (species === 'perro') {
+            editPetBreedDog.parentElement.style.display = 'block';
+            editPetBreedCat.parentElement.style.display = 'none';
+        } else if (species === 'gato') {
+            editPetBreedDog.parentElement.style.display = 'none';
+            editPetBreedCat.parentElement.style.display = 'block';
+        } else {
+            editPetBreedDog.parentElement.style.display = 'none';
+            editPetBreedCat.parentElement.style.display = 'none';
+        }
+    });
+
+    // Función para actualizar las razas en función de la especie seleccionada en el formulario de agregar mascota
+    newPetSpecies.addEventListener('change', function() {
+        const species = newPetSpecies.value;
+
+        if (species === 'perro') {
+            newPetBreedDog.parentElement.style.display = 'block';
+            newPetBreedCat.parentElement.style.display = 'none';
+        } else if (species === 'gato') {
+            newPetBreedDog.parentElement.style.display = 'none';
+            newPetBreedCat.parentElement.style.display = 'block';
+        } else {
+            newPetBreedDog.parentElement.style.display = 'none';
+            newPetBreedCat.parentElement.style.display = 'none';
+        }
+    });
 });
 
 ////////////////////////////////////////////////////////////////
@@ -404,3 +511,94 @@ function openModal(descriptionKey) {
 function closeModal() {
     document.getElementById('modal').classList.add('hidden');
 }
+
+//carnet digital
+function showTab(tabId) {
+    // Oculta todas las pestañas
+    document.querySelectorAll('.tab-content').forEach(tab => tab.classList.add('hidden'));
+    // Muestra la pestaña seleccionada
+    document.getElementById(tabId).classList.remove('hidden');
+
+    // Estilo para pestañas activas
+    document.querySelectorAll('.tab-button').forEach(button => button.classList.remove('active'));
+    document.querySelector(`button[onclick="showTab('${tabId}')"]`).classList.add('active');
+}
+
+// Efecto hover para filas de la tabla
+document.querySelectorAll('table tbody tr').forEach(row => {
+    row.addEventListener('mouseover', () => {
+        row.style.backgroundColor = '#f0f0f0'; // Color de fondo al pasar el ratón
+    });
+    row.addEventListener('mouseout', () => {
+        row.style.backgroundColor = ''; // Restaurar el color de fondo original
+    });
+});
+
+// Función para mostrar el modal con la descripción completa
+function openModal(descriptionId) {
+    const descriptions = {
+        'descripcion1': 'Revisión completa de salud. Chequeo de peso, temperatura y estado general. Se verificó la condición física general, se realizaron pruebas de diagnóstico para detectar posibles problemas y se ofrecieron recomendaciones para el cuidado continuo. Este control es fundamental para asegurar que la mascota se mantenga en óptimas condiciones de salud a lo largo del tiempo.',
+        'descripcion2': 'Limpieza dental y revisión de encías. Se realizó una limpieza exhaustiva de los dientes y se examinaron las encías para detectar signos de enfermedades dentales. El veterinario también proporcionó consejos sobre cómo mantener una buena higiene dental en el hogar. La salud dental es crucial para prevenir problemas futuros y mantener la boca de la mascota en buen estado.'
+        // Agrega más descripciones según sea necesario
+    };
+    
+    document.getElementById('modal-description').textContent = descriptions[descriptionId];
+    document.getElementById('modal').classList.remove('hidden');
+}
+
+// Función para cerrar el modal
+function closeModal() {
+    document.getElementById('modal').classList.add('hidden');
+}
+
+// Configuración de paginación
+const rowsPerPage = 5;
+let currentPage = 1;
+
+function paginateTable(tableId, rowsPerPage) {
+    const table = document.querySelector(`#${tableId} table`);
+    const rows = table.querySelectorAll('tbody tr');
+    const totalPages = Math.ceil(rows.length / rowsPerPage);
+
+    function showPage(page) {
+        rows.forEach((row, index) => {
+            row.style.display = (index >= (page - 1) * rowsPerPage && index < page * rowsPerPage) ? '' : 'none';
+        });
+    }
+
+    function createPaginationControls() {
+        const pagination = document.querySelector(`#${tableId}-pagination`);
+        pagination.innerHTML = '';
+
+        for (let i = 1; i <= totalPages; i++) {
+            const button = document.createElement('button');
+            button.textContent = i;
+            button.classList.add('pagination-button');
+            if (i === currentPage) {
+                button.classList.add('active');
+            }
+            button.addEventListener('click', () => {
+                currentPage = i;
+                showPage(currentPage);
+                updatePaginationControls();
+            });
+            pagination.appendChild(button);
+        }
+    }
+
+    function updatePaginationControls() {
+        const buttons = document.querySelectorAll(`#${tableId}-pagination .pagination-button`);
+        buttons.forEach(button => button.classList.remove('active'));
+        buttons[currentPage - 1].classList.add('active');
+    }
+
+    showPage(currentPage);
+    createPaginationControls();
+}
+
+// Llama a paginateTable para cada tabla
+document.addEventListener('DOMContentLoaded', () => {
+    paginateTable('vacunas', rowsPerPage);
+    paginateTable('desparasitaciones', rowsPerPage);
+    paginateTable('controles', rowsPerPage);
+});
